8a824999ef7b77ac1aac5eb95735d6124c410e70
Maciel Vigneau <macielvigneau@gmail.com>
2026-04-13 20:58:07 -0300

    Version 0.1
    - ASe añaden los archivos json y sus dependencias, express y archivos ".ejs".
    - Se añadio Estilos para todas la paginas .
    - Se añaden paginas bajo el archivo ".ejs" como cheack, el registro, products, login, carrito, el footer y el header.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/products', (req, res) => res.render('pages/product'));
+app.get('/cart', (req, res) => res.render('pages/cart'));
+app.get('/checkout', (req, res) => res.render('pages/checkout'));
+app.get('/login', (req, res) => res.render('pages/login'));
+app.get('/register', (req, res) => res.render('pages/register'));
+
+app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
