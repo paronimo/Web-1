2c39777892a852fa2bd2a2e6fae95f1fe36c3860
Maciel Vigneau <macielvigneau@gmail.com>
2026-04-28 15:00:48 -0300

    Version 0.5
    *se añaden estilos.
    *Se añade botones al carrito.
    * Se añade una base dedatos poco convencional.
    
    Co-Authored-By: tertorex <202159086+tertorex@users.noreply.github.com>

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/login', (req, res) => res.render('pages/login'));
 app.get('/products', (req, res) => res.render('pages/product'));
 app.get('/cart', (req, res) => res.render('pages/cart'));
 app.get('/checkout', (req, res) => res.render('pages/checkout'));
-app.get('/login', (req, res) => res.render('pages/login'));
+app.get('/', (req, res) => res.render('pages/index'));
 app.get('/register', (req, res) => res.render('pages/register'));
 
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
